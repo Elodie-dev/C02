79d2b4ea7934d1dcb07e9481e613fd95d0d76cb5
Elodie De Cola <ede-cola@bess-f4r3s11.clusters.42paris.fr>
2023-09-13 13:38:24 +0200

    corr ex09

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: ede-cola <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 09:26:41 by ede-cola          #+#    #+#             */
-/*   Updated: 2023/09/13 13:31:41 by ede-cola         ###   ########.fr       */
+/*   Updated: 2023/09/13 13:38:00 by ede-cola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ char	*ft_strcapitalize(char *str)
 				str[i] = str[i] - 32;
 			else if (str[i - 1] < 48 || (str[i - 1] > 57 && str[i - 1] < 65))
 				str[i] = str[i] - 32;
-			else if ((str[i - 1] > 90 && str[i - 1] < 96) || str[i - 1] > 122)
+			else if ((str[i - 1] > 90 && str[i - 1] < 97) || str[i - 1] > 122)
 				str[i] = str[i] - 32;
 		}
 		i++;
